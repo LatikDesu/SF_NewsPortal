624e366a6669867a828073f305d640fae4113386
LatikDesu <evga.abrosimov@gmail.com>
2023-04-02 13:37:42 +0300

    templatetags news

---
+++ news/templatetags/news_tags.py
@@ -0,0 +1,15 @@
+from django import template
+from news.models import Category, Post
+
+register = template.Library()
+
+
+@register.simple_tag
+def get_categories():
+    return Category.objects.all()
+
+
+@register.inclusion_tag('news/tags/last_news.html')
+def get_last_posts(count=3):
+    posts = Post.objects.order_by("id")[:count]
+    return {"last_posts": posts}
